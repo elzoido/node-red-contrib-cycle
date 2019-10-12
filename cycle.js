37a66246f42c531b15f752cdd4e6bb5e4fe65eb8
Sebastian Sproesser <elzoido@elbloggo.de>
2019-10-12 15:53:14 +0200

    First checkin

---
+++ cycle.js
@@ -0,0 +1,17 @@
+module.exports = function(RED) {
+    function CycleNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        node.on('input', function(msg) {
+            var nodeContext = this.context();
+            var conf_outputs = parseInt(config.outputs);
+            var out = new Array(conf_outputs);
+            var output = nodeContext.get('output') || 0;
+            out[output] = msg;
+            output = (output + 1) % conf_outputs;
+            nodeContext.set('output', output);
+            node.send(out);
+        });
+    }
+    RED.nodes.registerType("cycle",CycleNode);
+}
