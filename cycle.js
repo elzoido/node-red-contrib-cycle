bc1f82eb55205a4f3843abdb4f0f2950f9220095
Sebastian Sproesser <elzoido@elbloggo.de>
2020-09-09 12:02:13 +0200

    Bugfix

--- cycle.js
+++ cycle.js
@@ -6,20 +6,34 @@ module.exports = function(RED) {
             var nodeContext = this.context();
             var conf_outputs = parseInt(config.outputs);
             var out = new Array(conf_outputs);
-            var output = nodeContext.get('output') || 0;
+            var output;
+            if (nodeContext.get('output') === undefined) {
+                output = undefined;
+            } else {
+                output = nodeContext.get('output');
+            }
+             
             if (msg.hasOwnProperty("reset")) {
-                nodeContext.set('output', 0);
+                nodeContext.set('output', undefined);
             } else {
                 var skip = 1;
-                if (msg.hasOwnProperty('skip') && Number.isInteger(msg.skip.STATE)) {
-                    skip = msg.skip.STATE;
+                if (msg.hasOwnProperty('skip') && Number.isInteger(msg.skip)) {
+                    skip = msg.skip;
                 }
-                out[output] = msg;
-                output += skip;
+                if (output === undefined) {
+                    if (skip > 0) {
+                        output = skip - 1;
+                    } else {
+                        output = skip;
+                    }
+                } else {
+                    output += skip;
+                }
                 while (output < 0) {
                     output += conf_outputs;
                 }
                 output %= conf_outputs;
+                out[output] = msg;
                 nodeContext.set('output', output);
                 node.send(out);
             }
